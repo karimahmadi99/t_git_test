22ef1668e92ab725c4031f4984639a33c2e7078f
karim ahmadi <karimahmadi9900p@gmail.com>
2024-02-08 21:00:49 +0330

    hello te

---
+++ te.py
@@ -0,0 +1,13 @@
+def ger(f_name, l_name):
+    print(f_name + " " + l_name)
+
+rr = ger('dkdkd', 'eeeee')
+print(rr)
+
+while True:
+    o = input("what: ")
+    print(o)
+    if o == 'no':
+        break
+
+print("hello")
